d367dec7130a6ba1190b7a4a9fb25469acc7a9b4
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-09-16 00:01:01 +0530

    LeetCode Problem 191

---
+++ src/LeetCode_Problem_191.java
@@ -0,0 +1,20 @@
+public class LeetCode_Problem_191 {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(10111));
+    }
+
+    // approach is Right
+    // try to run no leet Code.
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0; i<32; i++){
+            if((n & 1) == 1){
+                count++;
+            }
+            n >>=1;
+        }
+        return count;}
+
+
+}
