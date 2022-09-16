eb7de9b63e90706a70696d4ae094966813e9684d
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-09-17 01:04:27 +0530

    LeetCode Problem 231

--- src/LeetCode_Problem_231.java
+++ src/LeetCode_Problem_231.java
@@ -5,7 +5,7 @@ public class LeetCode_Problem_231 {
 
     public static boolean isPowerOfTwo(int n) {
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= 30; i++) {
             int answer = (int) Math.pow(2,i);
             if (answer == n) {
                 return true;
