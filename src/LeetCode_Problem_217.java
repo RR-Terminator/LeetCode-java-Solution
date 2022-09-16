f647dd1e555dd37ddc5fb53d9377668b54680fda
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-09-17 01:04:47 +0530

    LeetCode Problem 217

--- src/LeetCode_Problem_217.java
+++ src/LeetCode_Problem_217.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class LeetCode_Problem_217 {
-}
+import java.util.Arrays;
+
+public class LeetCode_Problem_217 {
+    public static void main(String[] args) {
+
+        int [] arr = {1,2,3,4};
+        System.out.println(containsDuplicate(arr));
+
+    }
+    public static boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    }
+
+
+
+
+
+
+
+
