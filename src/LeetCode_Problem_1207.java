823a1ef4fee3321d6e9ca28a35d795422a4aa386
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-09-18 23:44:52 +0530

    LeetCode Problem 1207

---
+++ src/LeetCode_Problem_1207.java
@@ -0,0 +1,23 @@
+
+
+/*
+
+        Input: arr = [1,2,2,1,1,3]
+        Output: true
+        Explanation: The value 1 has 3 occurrences,
+        2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+ */
+
+
+public class LeetCode_Problem_1207 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+}
