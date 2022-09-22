5dd5eb24fa588262ba146265b99962cc32aa027a
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-09-22 21:49:40 +0530

    Leet Code Problem 374

---
+++ src/LeetCode_Problem_374.java
@@ -0,0 +1,34 @@
+/// Leet Code 374
+/// Works on leet Code Editor as spi call Of Guess number
+/// use binary Search To Guess Number is in a search space of 1 to n and is ina increasing order
+
+
+
+public class LeetCode_Problem_374 {
+    public static void main(String[] args) {
+
+    }
+
+    public int guessNumber(int n) {
+        int s = 0;
+        int e = n;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(guess(mid)==0){
+                return mid;
+            }
+            else if(guess(mid)==-1){
+                e = mid -1;
+            }
+            else {
+                s = mid+1;
+            }
+
+        }
+        return -1;
+    }
+
+
+
+}
