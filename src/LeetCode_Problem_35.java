7c407d36e48491feafb0907e0159bc35b6b73473
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-09-27 09:10:53 +0530

    Leet Code Problem 35

--- src/LeetCode_Problem_35.java
+++ src/LeetCode_Problem_35.java
@@ -1,7 +1,7 @@
 public class LeetCode_Problem_35 {
     public static void main(String[] args) {
         int [] arr = {1,3,5,7};
-        System.out.println(searchInsert(arr,5));
+        System.out.println(searchInsert(arr,2));
 
     }
     public static int  searchInsert(int[] nums, int target) {
