597f6fb83fd7703ae07c9e99dac3068abc31fb38
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-12-12 23:23:42 +0530

    leet code problem 231 solution recursive  added

--- src/LeetCode_Problem_231.java
+++ src/LeetCode_Problem_231.java
@@ -1,26 +1,44 @@
 public class LeetCode_Problem_231 {
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(15));
+        System.out.println(isPowerOfTwo(0));
     }
+
+//    public static boolean isPowerOfTwo(int n) {
+//
+//        for (int i = 0; i <= 30; i++) {
+//            int answer = (int) Math.pow(2,i);
+//            if (answer == n) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//
+//    }
+
+    // Recursive solution :-
 
     public static boolean isPowerOfTwo(int n) {
 
-        for (int i = 0; i <= 30; i++) {
-            int answer = (int) Math.pow(2,i);
-            if (answer == n) {
-                return true;
-            }
+        //Base Case : if n = 0 return false
+        if (n == 0) {
+            return false;
+        }
+        // Another base case if n = 1 return true as 2^0 = 1
+
+        if (n == 1) {
+            return true;
         }
 
-        return false;
+//         check if n%2 = 0 as 2 powers are always even number their division will give a number which is even
+//         consider case of 18 18%2 == 0 and 18/2 = 9 and 9%2!=0 hence return false
+//         eg 16 16%2 = 0 ,16/2 = 8 ,8%2 =0 ,8/2 =4 ,4%2 = 0, 4/2 = 2, 2%2 = 0 ,2/2 = 1 return true
+        if (n%2 == 0) {
+            return isPowerOfTwo(n/2);
+
+        }
+        else return false;
 
     }
-
-//    public  boolean isPowerOfTwo(int n) {
-//        if(n<=0)
-//            return false;
-//        return (n & n-1)==0;
-//
-//    }
 
 }
--- src/LeetCode_Problem_326.java
+++ src/LeetCode_Problem_326.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class LeetCode_Problem_326 {
+public class LeetCode_Problem_326 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(16));
+    }
+    public static boolean isPowerOfThree(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+
+        if (n%3 == 0) {
+            return isPowerOfThree(n/3);
+
+        }
+        else return false;
+    }
 }
