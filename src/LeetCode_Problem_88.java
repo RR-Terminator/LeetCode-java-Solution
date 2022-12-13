d45cdcfa6144c1b744b2aa4722c78a330b8c1687
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-12-13 16:59:19 +0530

    leet code problem 88 solution added

---
+++ src/LeetCode_Problem_88.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class LeetCode_Problem_88 {
+    public static void main(String[] args) {
+
+        int [] nums1 = {1,2,3,0,0,0};
+        int [] nums2 = {2,5,6};
+        merge(nums1,3,nums2,3);
+        System.out.println(Arrays.toString(nums1));
+
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1, p2 = n-1, i = m+n-1;
+        while(p2 >= 0){
+            if(p1 >= 0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            }
+            else {
+                nums1[i--] = nums2[p2--];
+            }
+        }
+    }
+}
