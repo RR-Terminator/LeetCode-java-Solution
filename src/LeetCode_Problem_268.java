f9162f766c6a8ddc16fda616d83d4c7bb9d3c93b
RR-Terminator <rahulkumarsr20it@student.mes.ac.in>
2022-12-13 21:02:12 +0530

    leet code problem 268 solution added

--- src/LeetCode_Problem_268.java
+++ src/LeetCode_Problem_268.java
@@ -1,14 +1,36 @@
-//public class LeetCode_Problem_268 {
-//
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    public static int missingNumber(int[] nums) {
-//
-//        int i = 0;
-//
-//    }
-//    public static Swap ()
-//
-//}
+public class LeetCode_Problem_268 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,0,1};
+        System.out.println(missingNumber(arr));
+
+    }
+
+    public static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correctIndex]) {
+                Swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+
+        return nums.length;
+    }
+
+
+    public static void Swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
